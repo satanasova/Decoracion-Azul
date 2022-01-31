4f40ad1e7d0188711e3dc2d58ef862f28b387523
satanasova <s.e.atanasowa@gmail.com>
2022-01-31 17:10:23 +0200

    added filter functionality to blog-page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,49 @@
-const header = document.querySelector('.header');
+import '../../node_modules/jquery/dist/jquery.min.js';
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY >= 95) {
-        header.classList.add('bg-solid')
-    } else {
-        header.classList.remove('bg-solid')
-    }
-})
+function headerOpacity() {
+    const header = document.querySelector('.header');
+
+    window.addEventListener('scroll', ()=>{
+        if(window.scrollY >= 95) {
+            header.classList.add('bg-solid')
+        } else {
+            header.classList.remove('bg-solid')
+        }
+    })
+}
+
+function filterBlogPost(blogContainerId) {
+    const blogContainer = $(`#${blogContainerId}`);
+    const blogTabsContainer = $(blogContainer).find('.categories');
+    const blogPostsContainer = blogContainer.find('.blog-posts-container .wrapper');
+    const blogPosts = blogPostsContainer.find('.single-post');
+
+    $(blogTabsContainer).on('click', (e) => {
+        const target = e.target;
+        
+        if($(target).hasClass('categories-tab')){
+            if($(target).hasClass('active')){
+                return;
+            } else {
+                $('.categories-tab').each((idx,el) => {
+                    $(el).removeClass('active');
+                })
+                $(target).addClass('active');
+    
+                blogPostsContainer.empty();
+    
+                const filterCondition = $(target).attr('data-id');
+                const filteredPosts = blogPosts.filter((idx,post) => {
+                    return $(post).attr('data-category') === filterCondition;
+                }) 
+    
+                const postsToAppend = filteredPosts.length > 0 ? filteredPosts : blogPosts;
+                postsToAppend.appendTo(blogPostsContainer).hide().fadeIn(500);
+    
+            }
+        }
+    });
+}
+
+headerOpacity();
+filterBlogPost('blog-posts-home');
